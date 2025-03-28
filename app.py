c037e2741f497f71a94d957b762c38a4392d8bc1
jorgeh18 <jorgehsmm@hotmail.com>
2025-03-28 14:07:04 +0100

    feat: add pokemon characteristics and description

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os, requests, requests_cache, random, datetime
 
 app = Flask(__name__)
 
-requests_cache.install_cache('app_cache', backend='sqlite', expire_after=300)
+requests_cache.install_cache('app_cache', backend='sqlite', expire_after=360)
 
 def get_pokemon_data(pokemon_reference):
     url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_reference}"
@@ -17,6 +17,12 @@ def get_pokemon_species_data(pokemon_reference):
     pokemon_species_data = response.json()
     return pokemon_species_data
 
+def get_pokemon_ability(ability_reference):
+    url = f"https://pokeapi.co/api/v2/ability/{ability_reference}"
+    response = requests.get(url)
+    ability_data = response.json()
+    return ability_data
+
 def get_latest_english_flavor_text(pokemon_species_data):
     flavor_text_entries = pokemon_species_data.get("flavor_text_entries", [])
     # Filter entries that are in English
@@ -24,6 +30,14 @@ def get_latest_english_flavor_text(pokemon_species_data):
     # Assuming the last English entry is the latest
     latest_flavor_text = english_entries[-1]['flavor_text'].replace('\n', ' ').replace('\f', ' ')
     return latest_flavor_text
+
+def get_pokemon_species_genus(pokemon_species_data):
+    pokemon_species_genus = pokemon_species_data.get("genera", [])
+    # Filter entries that are in English
+    english_entries = [entry for entry in pokemon_species_genus if entry['language']['name'] == 'en']
+    # Assuming the last English entry is the latest
+    species_genus = english_entries[-1]['genus']
+    return species_genus
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -36,6 +50,7 @@ def index():
     pokemon_data=get_pokemon_data(daily_pokemon_id)
     pokemon_species_data = get_pokemon_species_data(daily_pokemon_id)
     pokemon_flavor_text = get_latest_english_flavor_text(pokemon_species_data)
+    pokemon_species_genus = get_pokemon_species_genus(pokemon_species_data)
 
     # If search is done get the data from the searched pokemon
     if request.method == 'POST':
@@ -44,8 +59,9 @@ def index():
             pokemon_data=get_pokemon_data(pokemon_reference)
             pokemon_species_data = get_pokemon_species_data(pokemon_reference)
             pokemon_flavor_text = get_latest_english_flavor_text(pokemon_species_data)
-    
-    return render_template('index.html', pokemon=pokemon_data, pokemon_species=pokemon_species_data, flavor_text=pokemon_flavor_text)
+            pokemon_species_genus = get_pokemon_species_genus(pokemon_species_data)
+
+    return render_template('index.html', pokemon=pokemon_data, pokemon_species=pokemon_species_data, flavor_text=pokemon_flavor_text, genus=pokemon_species_genus)
 
 if __name__ == '__main__':
     app.run(debug=True)
